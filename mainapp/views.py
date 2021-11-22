abfa7976601938bb7ec1aadbc9ddcbba2a1c833f
rseitov <rseitov93@yandex.ru>
2021-11-22 17:26:04 +0400

    add json

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,21 +1,56 @@
 from django.shortcuts import render
+from django.templatetags.static import static
+import os
+import json
 
 # Create your views here.
 
-def index(request):
-    context = {'title':'Магазин'}
-    return render(request, 'mainapp/index.html',context)
+module_dir = os.path.dirname(__file__,)
 
-def products(request):
-    links_menu = [
+links_menu = [
         {'href': 'products_all', 'name': 'все'},
         {'href': 'products_home', 'name': 'дом'},
         {'href': 'products_office', 'name': 'офис'},
         {'href': 'products_modern', 'name': 'модерн'},
         {'href': 'products_classic', 'name': 'классика'},
     ]
-    return render(request, 'mainapp/products.html',{'links_menu':links_menu})
+
+menu = [
+        {'href': 'index', 'name': 'главная'},
+        {'href': 'products', 'name': 'продукты'},
+        {'href': 'contact', 'name': 'контакты'},
+]
+
+def index(request):
+    context = {'title':'Магазин', 'menu':menu}
+    return render(request, 'mainapp/index.html',context)
+
+def products(request):
+    file_path = os.path.join(module_dir,'fixtures/products.json')
+    products = json.load(open(file_path, encoding='utf-8'))
+
+    # products = [
+    #
+    #     {'name': 'Стул повышенного качества', 'text':'Не оторваться.', 'img': static('img/product-1.jpg')},
+    #     {'name': 'Стул повышенного качества', 'text':'Не оторваться.','img': static('img/product-2.jpg')},
+    #     {'name': 'Стул повышенного качества', 'text':'Не оторваться.','img': static('img/product-3.jpg')},
+    #     {'name': 'Стул повышенного качества', 'text':'Не оторваться.','img': static('img/product-4.jpg')},
+    #     {'name': 'Стул повышенного качества', 'text':'Не оторваться.','img': static('img/product-21.jpg')},
+    #     {'name': 'Стул повышенного качества', 'text':'Не оторваться.','img': static('img/product-11.jpg')},
+    # ]
+
+    content = {
+            'title': 'Продукты',
+            'links_menu': links_menu,
+            'products':products,
+            'menu':menu,
+        }
+
+
+
+    return render(request, 'mainapp/products.html',content)
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {'title': 'Контакты', 'menu': menu}
+    return render(request, 'mainapp/contact.html',context)
 

ab1b6a90cb47ee61c2aa7b294c1db8cefe58a524
rseitov <rseitov93@yandex.ru>
2021-11-22 14:33:56 +0400

    context

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,10 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {'title':'Магазин'}
+    return render(request, 'mainapp/index.html',context)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+    return render(request, 'mainapp/products.html',{'links_menu':links_menu})
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
+
